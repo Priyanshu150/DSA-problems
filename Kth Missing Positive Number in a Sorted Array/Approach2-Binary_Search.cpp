413aef3308cdb20d06080748481be4172efa7963
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2024-12-20 00:04:13 +0530

    Max Chunks To Make Sorted Binary Serach approach

--- Kth Missing Positive Number in a Sorted Array/Approach2-Binary_Search.cpp
+++ Kth Missing Positive Number in a Sorted Array/Approach2-Binary_Search.cpp
@@ -4,17 +4,14 @@ using namespace std;
 class Solution {
   public:
     int kthMissing(vector<int> &arr, int k) {
-        // Your code goes here
-        if(k < arr[0])      return k;
         int low = 0, high = arr.size()-1;
         
         while(low <= high){
             int mid = low + (high - low)/2;
-            int missing = abs(k - mid - 1);
+            int missing = arr[mid] - mid - 1;
             if(missing < k)     low = mid+1;
             else        high = mid-1;
         }
-        int more = k - high - 1;
-        return arr[high] + more;
+        return low + k; //high + 1 + k
     }
 };
