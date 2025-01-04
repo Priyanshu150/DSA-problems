d7343c53c0717d9bf3b3651b2a5eade8691bd49b
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-01-04 23:16:55 +0530

    Diffk

--- Diffk/Two_Pointer_Approach.cpp
+++ Diffk/Two_Pointer_Approach.cpp
@@ -1,24 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//Time complexity :- O(N*log(N))       
+//Time complexity :- O(N)       
 //Space complexity :- O(1)  
 
-int solve(vector<int> &arr, int target) {
-    int n = arr.size();
-    sort(arr.begin(), arr.end());
-    
-    if(target < 0)      target *= -1;
-    int i=0, j=1;
+int diffPossible(vector<int> &arr, int target) {
+    int n = arr.size(), i = 0, j = 1;
     
     while(j<n){
         int diff = arr[j] - arr[i];
         if(diff == target)
             return 1;
-        if(diff < target)
+        else if(diff < target)
             j++;
         else    i++;
-        
+            
         if(i == j)
             j++;
     }
