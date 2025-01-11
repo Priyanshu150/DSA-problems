763a23d62aa4a5645f16baa553f7016423510fd6
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-01-11 16:05:35 +0530

    Longest substring with distinct characters

---
+++ Two pointer/Longest substring with distinct characters/Sliding_Window.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Time complexity :- O(N)       
+//Space complexity :- O(26)    
+
+//Approach :-
+//Use sliding window to check which window for valid window
+//calculate the freq of each character 
+
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s) {
+        // code here
+        int n = s.size(), res = 1, j(0);
+        int freq[26] = {0};
+        
+        for(int i=0; i<n; ++i){
+            freq[s[i] - 'a'] += 1;
+            
+            while(j<i && freq[s[i] - 'a'] > 1){
+                freq[s[j] - 'a'] -= 1;
+                j++;
+            }
+            res = max(res, i-j+1);
+        }
+        return res;
+    }
+};
