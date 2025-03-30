b220b1be2cd17ae19df45f606132f377bc20ce46
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-03-30 12:48:34 +0530

    Partition Labels

---
+++ String/Partition Labels/Brute_force_approach.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Time complexity :- O(n*n*log(n))     // Where n is length of string s
+//Space complexity :- O(n+n)          // for freq array and set 
+
+//Approach :-
+// count the freq of each char in string 
+// Again traverse on the string and put each char in the set and decrease the freq of char 
+// reapet this until the char present in the set has 0 freq (inner loop)
+// once inner loop is completed push the length into answer vector 
+
+
+class Solution {
+private:
+    bool check(unordered_set<char> chars, vector<int> &freq){
+        for(auto ch: chars){
+            if(freq[ch - 'a'] > 0)
+                return true;
+        }
+        return false;
+    }
+
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> ans, freq(26, 0);
+
+        for(int i=0; i<n; ++i)
+            freq[s[i]-'a'] += 1;
+        
+        int i = 0, prev = i;
+        unordered_set<char> chars;
+
+        while(i < n){
+            chars.insert(s[i]);
+            freq[s[i] - 'a'] -= 1;
+            i++;
+
+            while(i<n && check(chars, freq)){
+                chars.insert(s[i]);
+                freq[s[i] - 'a'] -= 1;
+                i++;
+            }
+            ans.push_back(i - prev);
+            prev = i;
+            chars.clear();
+        }
+        return ans;
+    }
+};
---
+++ String/Partition Labels/Hashing_approach.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time complexity :- O(n)            // Where n is length of string s
+// Space complexity :- O(n)          // for array 
+
+// Approach :-
+// Capture the last index of each char in array
+// traverse on the string again and keep the last of the currnt_char (last)
+// In inner loop, keep updating the last index depending on the incoming chars 
+// once inner loop traversal is completed store the lenght in the ans
+
+
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ans, lastInd(26, -1);
+        int n = s.size(), ind = 0, prevInd = 0;
+
+        for(int i=0; i<n; ++i)
+            lastInd[s[i]-'a'] = i;
+        
+        while(ind < n){
+            int last = lastInd[s[ind]-'a'];
+            ind++;
+
+            while(ind <= last){
+                last = max(last, lastInd[s[ind]-'a']);
+                ind++;
+            }
+            ans.push_back(ind - prevInd);
+            prevInd = ind;
+        }
+        return ans;
+    }
+};
