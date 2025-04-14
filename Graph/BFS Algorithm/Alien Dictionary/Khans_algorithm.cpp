76ec77e169ff649689f796a72603983b494c48e2
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-04-14 18:52:41 +0530

    Alien Dictionary

---
+++ Graph/BFS Algorithm/Alien Dictionary/Khans_algorithm.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time complexity :- O(n)
+// Space complexity :- O(n)
+
+// Approach :-
+// Using the words given create a directed graph on based on the logic
+// that 2 words differ at a index then, for ex those char are a & b then we can say that a -> b has a edge
+// after creating the graph apply khans algorithm to get the order
+// The order will not exist 1) if there is cyclic dependency and
+// 2) greater string appear before smaller string and they both differ by only 1 char
+// which is present at the last i.e. [abc, ab]
+
+
+// Links :- https://www.geeksforgeeks.org/problems/alien-dictionary/1
+
+class Solution {
+  public:
+    string findOrder(vector<string> &words) {
+        // code here
+        unordered_map<char, vector<char>> graph;
+        unordered_map<char, int> inDegree;
+        string prevWord = "", res = "";
+        
+        for(auto word: words){
+            int n = word.size(), m = prevWord.size();
+            
+            for(int i=0; i<n; ++i){
+                if(!inDegree.count(word[i]))
+                    inDegree[word[i]] = 0;
+                
+                if(prevWord != "" && i<m && prevWord[i] != word[i]){
+                    m = 0;
+                    graph[prevWord[i]].push_back(word[i]);
+                    inDegree[word[i]] += 1;
+                }
+            }
+            if(m > n)   return "";
+            prevWord = word;
+        }
+        queue<char> q;
+        
+        for(auto it: inDegree){
+            char nd = it.first;
+            int inDeg = it.second;
+            if(inDeg == 0)
+                q.push(nd);
+        }
+        
+        while(!q.empty()){
+            char nd = q.front();
+            q.pop();
+            
+            res += nd;
+            
+            for(auto ngr: graph[nd]){
+                inDegree[ngr] -= 1;
+                if(inDegree[ngr] == 0)
+                    q.push(ngr);
+            }
+        }
+        return (res.size() == inDegree.size()) ? res : "";
+    }
+};
