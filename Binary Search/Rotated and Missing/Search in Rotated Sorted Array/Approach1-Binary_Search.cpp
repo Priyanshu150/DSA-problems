6cec6533c2fddba26ddfe7830e67f3a2a32fe023
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-07-20 00:20:36 +0530

    Search in Rotated Sorted Array

--- Binary Search/Rotated and Missing/Search in Rotated Sorted Array/Approach1-Binary_Search.cpp
+++ Binary Search/Rotated and Missing/Search in Rotated Sorted Array/Approach1-Binary_Search.cpp
@@ -1,6 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Time complexity :- O(log(n))   
+//Space complexity :- O(1)
+
+// Approach :-
+// use standard binary search technique on index
+// if the mid is equal to target return from that point with mid as answer
+// otherwise there will be 2 case, if left or right part is sorted 
+// when left part is, then check wheather the number is lying in the sorted part if yes 
+// then high = mid-1 otherwise low = mid+1
+// when right part is, then check wheather the number is lying in the sorted part if yes 
+// then low = mid+1 otherwise high = mid-1
+
+
+//Link :- https://leetcode.com/problems/search-in-rotated-sorted-array/
+
 class Solution {
   public:
     int search(vector<int>& arr, int target) {
