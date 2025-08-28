6c4cdf098801efd5e985351c2b80763ea6062644
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-08-28 20:35:15 +0530

    Maximize Number of 1's

---
+++ Two Pointers/Outer Loop Result Calculation/Maximize Number of 1's/counting_approach.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Time complextiy :- O(n*log(n) + n*n*log(n))
+// Space complexity :- O(1)
+
+// Approach :-  
+// use sliding window approach find a valid window where at most k zeros are there 
+// maintain a result which keep the maximum result and store the maximum valid window size 
+// remove element from the left side of the window whenever a non-valid window is found 
+
+// link :- https://www.geeksforgeeks.org/problems/maximize-number-of-1s0905/1
+
+class Solution {
+  public:
+    int maxOnes(vector<int>& arr, int k) {
+        // code here
+        int  n = arr.size(), left = 0, zeros = 0, res = 0;
+        
+        for(int right=0; right<n; ++right){
+            if(arr[right] == 0)
+                zeros++;
+                
+            while(zeros > k){
+                if(arr[left] == 0)
+                    zeros--;
+                
+                ++left;
+            }
+            res = max(res, right - left + 1);
+        }
+        return res;
+    }
+};
