ebe8c1a222a6bcd886954028052be820d5d91cbc
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-11-01 23:03:27 +0530

    Kth Smallest Product of Two Sorted Arrays

--- Binary Search/Count element less than or Equal to/Kth Smallest Product of Two Sorted Arrays/counting_numbers_approach.cpp
+++ Binary Search/Count element less than or Equal to/Kth Smallest Product of Two Sorted Arrays/counting_numbers_approach.cpp
@@ -5,71 +5,76 @@ using namespace std;
 //Space complexity :- O(1)
 
 // Approach :-
-// we can use binary search and decide on the partition based on the the fact that 
-// how many number are smaller or equal to the mid value 
-// our range of potential answer will be -1e10 to 1e10 according to constraints 
-// use binary serach for count, traverse on the every element of one array
-// and apply binary search on the second array count the number of element smaller or equal to (nums1[i]*nums2[mid])
-// use two different binary search for possitive and negative array  
-// we can decide that based on the fact nums1[i] is negative or not 
+// we can use bs on answer for this problem 
+// for every number, we can check how many number are smaller or equal to that in the prod
+// if (cnt >= k)    increase low, otherwide decrease high 
+// for finding the count on number of product for each element we can again use binary search
+// we'll fix the one element from any array and apply bs on other array 
+// and count the number of element less than given mid value 
+// if (curr_val >= 0), then that means product of them will be sorted in left to right 
+// otherwise it will be sorted in right to left order 
 
 
-// link :- https://leetcode.com/problems/kth-smallest-product-of-two-sorted-arrays/
+// link :- https://leetcode.com/problems/maximize-the-minimum-powered-city/
 
 class Solution {
 private:
-    #define ll long long
+    typedef long long ll;
 
-    ll countSmallerOrEqual(ll num, vector<int>& nums1, vector<int>& nums2, int &n1, int &n2){
-        ll productCnt = 0;
+    bool isPossible(ll min_power, vector<ll> &total_power, int &n, int k, int &r){
+        vector<ll> extra_power(n+1, 0);
 
-        for(int ind=0; ind<n1; ++ind){
-            if(nums1[ind] >= 0){
-                int low = 0, high = n2-1, m = -1;
-                while(low <= high){
-                    int mid = low + (high - low)/2;
-                    ll product = 1LL* nums1[ind] * nums2[mid];
+        for(int i=0; i<n; ++i){
+            // extra power of curr station if any based on prev new power station
+            extra_power[i] += (i>0 ? extra_power[i-1] : 0);
+            // current power of the station 
+            ll curr_power = total_power[i] + extra_power[i];
+            // power required at current station 
+            ll required = max(0LL, min_power - curr_power);
+            if(required == 0)
+                continue;
 
-                    if(product <= num){
-                        low = mid+1;
-                        m = mid;
-                    }
-                    else                    high = mid-1;
-                }
-                productCnt += (m+1);
-            }
-            else{
-                int low = 0, high = n2-1, m = n2;
-                while(low <= high){
-                    int mid = low + (high - low)/2;
-                    ll product = 1LL * nums1[ind] * nums2[mid];
-
-                    if(product <= num){
-                        high = mid-1;
-                        m = mid;
-                    }
-                    else                    low = mid+1;
-                }
-                productCnt += (n2 - m);
-            }
+            // required power demand is more than k
+            if(required > k)
+                return false;
+            
+            // adding required power stations at current station
+            k -= required;
+            // considering current station as the end of range on left side 
+            extra_power[i] += required;
+            extra_power[min(n, i+2*r+1)] -= required;
         }
-        return productCnt;
+        return true;
     }
 
 public:
-    long long kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        ll low = -1e10, high = 1e10, res;
+    long long maxPower(vector<int>& stations, int r, int k) {
+        int n = stations.size();
+        vector<ll> total_power(n+1, 0);
 
+        for(int i=0; i<n; ++i){
+            // the current tower will contribute it's power from range {i-r, i+r+1}
+            total_power[max(0, i-r)] += stations[i];
+            total_power[min(n, i+r+1)] -= stations[i];
+        }
+        ll low = total_power[0], high = total_power[0]+k, res = 0;
+
+        // using difference array method calculate the total power 
+        for(int i=1; i<n; ++i){
+            total_power[i] += total_power[i-1];
+            low = min<ll>(low, total_power[i]);
+            high = max<ll>(high, total_power[i]+k);
+        }
+
+        // standard bs on answer 
         while(low <= high){
             ll mid = low + (high - low)/2;
-            // check if this is kth smallest or not 
-            ll cnt = countSmallerOrEqual(mid, nums1, nums2, n1, n2);
-            if(cnt >= k){
-                high = mid-1;
+            if(isPossible(mid, total_power, n, k, r)){
                 res = mid;
+                low = mid+1;
             }
-            else        low = mid+1;
+            else 
+                high = mid-1;
         }
         return res;
     }
