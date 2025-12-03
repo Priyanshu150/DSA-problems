be2ebaf82119fa180c15ac613729f5eb4836c364
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-12-03 08:59:39 +0530

    All Subset Xor Sum

--- Bit manupulation/XOR Problem/All Subsets Xor Sum/bit_property_app.cpp
+++ Bit manupulation/XOR Problem/All Subsets Xor Sum/bit_property_app.cpp
@@ -5,6 +5,14 @@ using namespace std;
 // Space complexity :- O(1)
 
 // Approach :-   
+// If we create every sequence and check then we'll find that, xorr of subset will be 
+// the bit which is set * the weight of that bit 
+// weight of that bit will be number of times it repeated
+// i.e. 2^(n-1) times because since we're creating a subset so every number will repeat that many times 
+// hence the bit are supposed to be repeat that many times 
+// For find which bit is set we can use 'OR' operation in the array 
+// and we can finally multiply our or result to the weight i.e. OR_Value * 2^(n-1)
+// or in other words can also be written as (OR_Value * 1<<(n-1)) and (OR_Value * 1<<(n-1))
 
 //Link :- https://www.geeksforgeeks.org/problems/sum-of-xor-of-all-possible-subsets/1
 
@@ -12,15 +20,15 @@ using namespace std;
 class Solution {
   public:
     int subsetXORSum(vector<int>& arr) {
-        int n = arr.size(), xorr = 0;
+        int n = arr.size(), orr = 0;
         
         // xor of array 
         for(auto num: arr){
-            xorr |= num;
+            orr |= num;
         }
         
         int times = (1 << (n-1));
-        return xorr << n-1;
+        return orr << n-1;
     }
 };
 
@@ -28,14 +36,14 @@ class Solution {
 class Solution {
   public:
     int subsetXORSum(vector<int>& arr) {
-        int n = arr.size(), xorr = 0;
+        int n = arr.size(), orr = 0;
         
         // xor of array 
         for(auto num: arr){
-            xorr |= num;
+            orr |= num;
         }
         
         int times = (1 << (n-1));
-        return xorr * (1<<(n-1));
+        return orr * (1<<(n-1));
     }
 };
