e5738fee2ae86c799c5247845df49df11e454c85
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-01-02 10:03:28 +0530

    Sort 0s, 1s and 2s

--- Array/Sorting/0-1-2 sort or Sort by color/Intutive_Approach.cpp
+++ Array/Sorting/0-1-2 sort or Sort by color/Intutive_Approach.cpp
@@ -13,21 +13,37 @@ using namespace std;
 // Traverse from mid to high and sort the value by swapping them to either to low or high
 // Intially mid = 0, high = n-1 which is the unknown region
 
-//link :- https://www.interviewbit.com/problems/sort-by-color/
+//link :- 
+// interviewbit - https://www.interviewbit.com/problems/sort-by-color/
+// gfg - https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
 
-void sortColors(vector<int> &A) {
-    int n = A.size(), low = 0, high = n-1, mid = 0;
-    
-    while(mid <= high){
-        if(A[mid] == 0){
-            swap(A[low], A[mid]);
-            mid++;
-            low++;
-        }
-        else if(A[mid] == 1)    mid++;
-        else{
-            swap(A[mid], A[high]);
-            high--;
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int n = arr.size(), low = 0, mid = 0, high = n-1;
+        
+        // 0 to low-1 indexes has all 0 number 
+        // low to mid-1 indexes has all 1 number
+        // mid to high is unknown region contins all the elements 
+        // (high+1) to (n-1) index has all 2 number 
+        
+        // traverse untill unknown region exists 
+        while(mid <= high){
+            if(arr[mid] == 0){
+                // swap the element at index mid and low and increase the mid ptr
+                swap(arr[mid], arr[low]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1){
+                // increase the 1's region by moving mid ptr
+                mid++;
+            }
+            else if(arr[mid] == 2){
+                // swap the numbers at index mid and high and decrement the high ptr
+                swap(arr[mid], arr[high]);
+                high--;
+            }
         }
     }
-}
+};
