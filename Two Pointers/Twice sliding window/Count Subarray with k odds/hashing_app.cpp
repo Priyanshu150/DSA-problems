3c75dfe83b0a339868b0533ec0cb20e9580d4818
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-01-11 22:01:04 +0530

    Count Subarray with k odds

--- Two Pointers/Twice sliding window/Count Subarray with k odds/hashing_app.cpp
+++ Two Pointers/Twice sliding window/Count Subarray with k odds/hashing_app.cpp
@@ -5,7 +5,8 @@ using namespace std;
 //Space complexity :- O(n)
 
 // Approach :-
-// 
+// using hashing approach, we can count the freq of odd number along the array 
+// and for every element, we'll count the how many freq (odd-k) exists 
 
 //link :- https://www.geeksforgeeks.org/problems/count-subarray-with-k-odds/1
 
@@ -14,16 +15,19 @@ class Solution {
     int countSubarrays(vector<int>& arr, int k) {
         int n = arr.size(), count = 0, odd = 0;
         
-        unordered_map<int,int> prefix;
-        prefix[0]= 1;
+        unordered_map<int,int> prefix;     // {odd_num_cnt, freq}
+        prefix[0]= 1;                      // 0 will have 1 as a count 
         
         for(int i=0; i<n; ++i){
+            // count the odd number 
             if(arr[i] % 2)
                 odd++;
             
+            // check if previously we've seen (odd - k)
             if(prefix.count(odd - k) != 0)
                 count += prefix[odd - k];
             
+            // odd number count update 
             prefix[odd]++;
         }
         return count;
