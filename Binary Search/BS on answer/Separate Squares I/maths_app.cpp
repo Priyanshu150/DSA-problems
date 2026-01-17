71b469863b447b6afcbc1583a16c1944b1f6bb53
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-01-17 22:21:16 +0530

    Separate Square I

---
+++ Binary Search/BS on answer/Separate Squares I/maths_app.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Time complextiy :-  O(n * log(range)), range = max (y+side) value - min y value
+// Space complexity :- O(1)
+
+// Approach :- 
+// using binary search, we can traverse on all the possible values of y 
+// and calculate the area below the line 
+// also we'll pre-calculate the area of the complete squares 
+// TopArea = (totalArea - bottomArea) or if both are equal then bottomArea = totalArea/2.0
+// 
+
+
+// Link :- https://leetcode.com/problems/separate-squares-i/
+
+class Solution {
+public:
+    bool check(vector<vector<int>>& squares, double mid_y, double total) {
+        double bot_area = 0;
+
+        for(auto &square : squares) {
+            double y = square[1];
+            double l = square[2];
+
+            double boty = y;
+            double topy = y + l;
+
+            if (mid_y >= topy) {
+                //full square below
+                bot_area += l * l;
+            } else if (mid_y > boty) {
+                //partial area below
+                bot_area += (mid_y - boty) * l;
+            }
+        }
+
+        return bot_area >= total/2.0; //Is bottom area more than above ?
+    }
+
+    double separateSquares(vector<vector<int>>& squares) {
+        double low   = INT_MAX;
+        double high  = INT_MIN;
+        double total = 0.00000;
+
+        for(auto &square : squares) {
+            double x = square[0];
+            double y = square[1];
+            double l = square[2];
+            total   += l*l;
+
+            low  = min(low, y);
+            high = max(high, y+l);
+        }
+
+        double result_y = 0.00000;
+
+        while(high-low > 1e-5) {
+            double mid_y = low + (high-low)/2;
+            
+            result_y = mid_y;
+
+            if (check(squares, mid_y, total) == true) { //bottom area is greater
+                //shift mid_y down side to balance area
+                high = mid_y;
+            } else {
+                low = mid_y;
+            }
+        }
+
+        return result_y;
+    }
+};
