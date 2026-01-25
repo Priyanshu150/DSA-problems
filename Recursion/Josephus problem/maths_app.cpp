f6d0be7e52deb88ef25c299a1095a49b3cfc2b19
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-01-25 23:51:51 +0530

    Josephus problem

---
+++ Recursion/Josephus problem/maths_app.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Time complextiy :- O()
+// Space complexity :- O()
+
+// Approach :-  
+// 
+
+class Solution {
+  public:
+    int josephus(int n, int k) {
+        if(n == 1)       return 1;
+       return   (josephus(n-1,k) + (k-1))%n +1;
+    }
+};
