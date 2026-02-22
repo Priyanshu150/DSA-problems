5623e720b131cc0ecc4c84b63551cf31151aa1fa
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-02-22 21:54:53 +0530

    Count Subarrays with given XOR

--- Hashing/Prefix Sum and Subarrays/Count Subarrays with given XOR/Prefix_Xorr_And_Hashing_Approach.cpp
+++ Hashing/Prefix Sum and Subarrays/Count Subarrays with given XOR/Prefix_Xorr_And_Hashing_Approach.cpp
@@ -14,15 +14,18 @@ using namespace std;
 class Solution {
   public:
     long subarrayXor(vector<int> &arr, int k) {
-        // code here
         unordered_map<long, long> mp;
+        // add 0 to hash map having contribution 1 
         mp[0] = 1;
         
         long xorr = 0, res = 0;
         
         for(auto num: arr){
+            // find the overall xor till current index  
             xorr ^= num;
-            res += mp[xorr ^ k];
+            // check (xorr ^ k), value exist or not 
+            res += mp[xorr ^ k];        // add the contribution of that into answer 
+            // update the contribution of current xor 
             mp[xorr] += 1;
         }
         return res;
