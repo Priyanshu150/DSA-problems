eda01b11acd57bcdee6ea03fe44051c106b8831e
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-05-10 03:02:30 +0530

    Max Profit from Two Machines

---
+++ Greedy/Custom Sorting/Max Profit from Two Machines/app1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+// Time complexity :- O(n * log(n))
+// Space complexity :- O(n)
+
+// Approach :-  
+// 
+
+
+// Links :- https://www.geeksforgeeks.org/problems/max-profit-from-two-machines/1
+
+class Solution {
+  public:
+    int maxProfit(int x, int y, vector<int> &arr, vector<int> &brr) {
+        // code here
+        int n = arr.size();
+        vector<vector<int> > diff;
+        
+        for(int i=0; i<n; ++i){
+            int absVal = abs(arr[i] - brr[i]);
+            diff.push_back({absVal, arr[i], brr[i]});
+        }
+        sort(diff.begin(), diff.end(), [&](vector<int> &v1, vector<int> &v2){
+            return v1[0] > v2[0];
+        });
+        long long res = 0;
+        
+        for(int i=0; i<n; ++i){
+            if(x == 0)          res += diff[i][2];
+            else if(y == 0)     res += diff[i][1];
+            else if(diff[i][1] > diff[i][2]){
+                res += diff[i][1];
+                x--;
+            }
+            else{
+                res += diff[i][2];
+                y--;
+            }
+        }
+        return res;
+    }
+};
---
+++ Greedy/Custom Sorting/Max Profit from Two Machines/app2.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+// Time complexity :- O(n * log(n))
+// Space complexity :- O(n)
+
+// Approach :-  
+// maintain a array with difference b/w two cost of robot doing task 
+// sort them based on the lower to higher difference value, except negative difference sort them by absolute value 
+// pick the next x trasaction from robot A and next y transaction from robot B 
+
+// Links :- https://www.geeksforgeeks.org/problems/max-profit-from-two-machines/1
+
+class Solution {
+  public:
+    int maxProfit(int x, int y, vector<int> &a, vector<int> &b) {
+        int n = a.size();
+        vector<pair<int, pair<int,int>>> temp;
+        
+        // add difference b/w task cost and task cost into array 
+        for(int i=0; i<n; ++i){
+            int diff = (a[i] - b[i]);
+            temp.push_back({diff, {a[i], b[i]}});
+        }
+        
+        // sort the array based on the differece 
+        sort(temp.begin(), temp.end(), [&](pair<int, pair<int,int>> &p1, pair<int, pair<int,int>> &p2){
+            int diff1 = p1.first;
+            int diff2 = p2.first;
+            
+            // sort the -ve diff based on higher absolute value 
+            if(diff1 < 0 && diff2 < 0)
+                return  diff1 < diff2;
+            
+            return diff1 > diff2;
+        });
+        
+        int maxProfit = 0;
+        
+        for(int i=0; i<n; ++i){
+            // find the different, cost of task from sorted values 
+            int diff = temp[i].first;
+            int cost1 = temp[i].second.first;
+            int cost2 = temp[i].second.second;
+            
+            // +ve differen and we can make machine A work
+            if(diff >= 0 && x > 0){
+                maxProfit += cost1;
+                x--;
+            }
+            // -ve differen and we can make machine B work
+            else if(y > 0){
+                maxProfit += cost2;
+                y--;
+            }
+            // otherwise machine A can work 
+            else{
+                maxProfit += cost1;
+                x--;
+            }
+        }
+        return maxProfit;
+    }
+};
