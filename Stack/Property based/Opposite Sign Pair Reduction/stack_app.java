0b851c249872b2fd91fa2aed9801fb07ff1dbd81
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-06-13 13:20:02 +0530

    Opposite Sign Pair Reduction

--- Stack/Property based/Opposite Sign Pair Reduction/stack_app.java
+++ Stack/Property based/Opposite Sign Pair Reduction/stack_app.java
@@ -3,47 +3,63 @@ import java.util.*
 // Time complexity :- O(n)
 // Space complexity :- O(n)
 
-// Approach :- 
+// Approach :-
+// we can use stack to store element into it
+// if stack is empty add the element directly
+// check if they have opposite polarity and remove all the smaller element from stack
+// remove both element if they has different sign and having same value 
+// add element if adjacent have same value or stack is empty
 
 
-// Links :- https://www.geeksforgeeks.org/problems/asteroid-collision/1
+// Link :- https://www.geeksforgeeks.org/problems/asteroid-collision/1
+// Lnik2 :- https://www.geeksforgeeks.org/problems/opposite-sign-pair-reduction/1
+
 
 class Solution {
+    // check if both the number has same polarity or not 
+    private boolean isSame(int num1, int num2){
+        // same sign
+        if(num1 > 0 && num2 > 0)
+            return true;
+        // same sign
+        if(num1 < 0 && num2 < 0)
+            return true;
+        // opposite sign
+        return false;
+    }
+    
     public ArrayList<Integer> reducePairs(int[] arr) {
-        ArrayList<Integer> s = new ArrayList<>();
-
-        // Traverse each element
-        for (int element : arr) {
-
-            // Process while there is a collision (opposite signs)
-            while (!s.isEmpty() && s.get(s.size() - 1) * element < 0) {
-                int top = s.get(s.size() - 1);
-
-                // Top has larger absolute value, keep top
-                if (Math.abs(top) > Math.abs(element)) {
-                    element = top;
-                    s.remove(s.size() - 1);
-                }
-
-                // Current element has larger absolute value
-                // keep current element
-                else if (Math.abs(top) < Math.abs(element)) {
-                    s.remove(s.size() - 1);
-                }
-
-                // Both have same absolute value, both removed
-                else {
-                    s.remove(s.size() - 1);
-                    element = 0;
-                    break;
-                }
+        int n = arr.length;
+        
+        Stack<Integer> st = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for(int i=0; i<n; ++i){
+            // check if they have opposite polarity and stack has smaller value 
+            while(!st.empty() && !isSame(st.peek(), arr[i]) && Math.abs(st.peek()) < Math.abs(arr[i])){
+                st.pop();
             }
-
-            // If current element still exists, push into stack
-            if (element != 0)
-                s.add(element);
+            // both has different sign and having same value 
+            if(!st.empty() && !isSame(st.peek(), arr[i]) && Math.abs(st.peek()) == Math.abs(arr[i])){
+                st.pop();
+                continue;
+            }
+            // same sign then add into the stack 
+            if(!st.empty() && isSame(st.peek(), arr[i])){
+                st.push(arr[i]);
+            }
+            // stack is empty then add it 
+            if(st.empty()){
+                st.push(arr[i]);
+            }
         }
-
-        return s;
+        // add the remaning element into answer 
+        while(!st.empty()){
+            res.add(st.peek());
+            st.pop();
+        }
+        // reverse the answer 
+        Collections.reverse(res);
+        return res;
     }
 }
