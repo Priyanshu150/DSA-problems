71b74e3f3db901952e51e8b69a61e8751882c6ce
Priyanshu Singh <priyanshukumarsingh1502@gmail.com>
2026-07-08 00:01:44 +0530

    Largest Unblocked Submatrix

--- Matrix/Greedy/Largest Unblocked Submatrix/sorting_app.java
+++ Matrix/Greedy/Largest Unblocked Submatrix/sorting_app.java
@@ -7,7 +7,7 @@ import java.util.*;
 // store the each row, col value in an array which are block
 // find the length and breadth b/w those block rows,cols, start and end 
 // maintain max width and length among all the value 
-// product of both will be our answer 
+// product of both will be largest area among all 
 
 //Link :- https://www.geeksforgeeks.org/problems/largest-unblocked-submatrix/1
 
@@ -17,18 +17,22 @@ class Solution {
         List<Integer> rows = new ArrayList<>();
         List<Integer> cols = new ArrayList<>();
         
+        // add the start cell 
         rows.add(0);
         cols.add(0);
+        // add the ending cell
         rows.add(n+1);
         cols.add(m+1);
         
         int k = arr.length;
         
+        // add block rows,cols into respective ds 
         for(int i=0; i<k; ++i){
             rows.add(arr[i][0]);
             cols.add(arr[i][1]);
         }
         
+        // sort both the array 
         Collections.sort(rows);
         Collections.sort(cols);
         
@@ -36,16 +40,18 @@ class Solution {
         int maxLength = 0;
         int maxBreadth = 0;
         
+        // find the largest length among all the lengths
         for(int i=1; i<rows.size(); ++i){
             int length = rows.get(i) - rows.get(i-1) - 1;
             maxLength = Math.max(maxLength, length);
         }
         
+        // find the largest breadth among all the breadths 
         for(int i=1; i<cols.size(); ++i){
             int breadth = cols.get(i) - cols.get(i-1) - 1;
             maxBreadth = Math.max(maxBreadth, breadth);
         }
-        
+        // area wil be length * breadth 
         return maxLength * maxBreadth;
     }
 }
